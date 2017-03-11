73d93ba1a7a994514ede40530b425143ac9552c2
Buck Young <buckcyoung@gmail.com>
2017-03-11 10:59:51 -0500

    Carry the torch minigame

--- Assets/Scripts/DistanceMinigame/DistanceMarker.cs
+++ Assets/Scripts/DistanceMinigame/DistanceMarker.cs
@@ -6,23 +6,57 @@ namespace Hallway.DistanceMinigame {
 	public class DistanceMarker : MonoBehaviour {
 	
 		private GameObject[] players;
+		private Player.Player[] playerScripts;
 		private float globalMaxDistance = 0.0f;
+		private Light myLight;
+		private GUIStyle guiStyle = new GUIStyle();
 
 		void Start() {
-			players = GameObject.FindGameObjectsWithTag("Player");
+			myLight = GetComponentInChildren<Light>();
+
+			GameObject[] taggedPlayers = GameObject.FindGameObjectsWithTag("Player");
+
+			players = new GameObject[taggedPlayers.Length];
+			playerScripts = new Player.Player[taggedPlayers.Length];
+
+			// Sort tagged players
+			for (int i = 0; i < taggedPlayers.Length; i++) {
+				Player.Player playerScript = taggedPlayers[i].GetComponent<Player.Player>();
+				int playerId = playerScript.getId();
+				int playerIndex = playerId - 1;
+
+				players[playerIndex] = taggedPlayers[i];
+				playerScripts[playerIndex] = taggedPlayers[i].GetComponent<Player.Player>();
+			}
+
+			// GUI Style
+			guiStyle.fontSize = 24;
+			guiStyle.normal.textColor = Color.black;
+			guiStyle.clipping = TextClipping.Clip;
 		}
 
 		void Update() {
 			float playerXPosition = 0.0f;
+			myLight.range = 10;
 
-			foreach (GameObject player in players) {
-				playerXPosition = player.transform.position.x;
+			for (int i = 0; i < players.Length; i++) {
+				playerXPosition = players[i].transform.position.x;
 
 				if (playerXPosition > globalMaxDistance) {
 					globalMaxDistance = playerXPosition;
 					this.transform.position = new Vector2(playerXPosition, -3.5f);
-				};
+					myLight.color = playerScripts[i].getTint();
+					myLight.range = 22;
+				}
 			}
+		}
+
+		void OnGUI() {
+			float dist = (float)(((int)(globalMaxDistance * 100)) / 10.0f);
+
+			Vector2 pos = (Vector2)Camera.main.WorldToScreenPoint(transform.position);
+
+			GUI.Label(new Rect(pos.x, Screen.height/3 * 2, 100, 100), dist.ToString(), guiStyle); 
 		}
 	}
 }
